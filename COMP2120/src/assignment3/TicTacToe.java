cb719975cbf00ffd61ea9a1579eb516df01396ab
HarrisonScarfone <hscarfone77@gmail.com>
2019-11-18 23:28:57 -0500

    backend of lab 9 done. gui still todo

--- COMP2120/src/assignment3/TicTacToe.java
+++ COMP2120/src/assignment3/TicTacToe.java
@@ -1,19 +1,5 @@
 package assignment3;
 
-/*- TicTacToe.java a simple wrapper class for main, here is its contents:
-
-class TicTacToe {
-
-   public static void main(String args[]) {
-
-     game myGame = new game();
-
-     myGame.start();
-
-    }
-
-}*/
-
 public class TicTacToe {
 
 	public static void main(String[] args) {
